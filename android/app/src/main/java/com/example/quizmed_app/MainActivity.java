6dc320fa2cc180332160ef494b8e25f456a34baf
blandonyerman7-maker <blandonyerman7@gmail.com>
2025-09-28 18:33:52 -0600

    Delete android directory

--- android/app/src/main/java/com/example/quizmed_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tuapp; // Asegúrate de que "tuapp" coincida con el nombre de tu carpeta
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
